791623c15c495e07454deb0fcff8c8abea22d603
Sharief smm <shariefsmm@gmail.com>
2020-03-03 17:38:14 +0530

    Full Disassembler

--- disasm.cpp
+++ disasm.cpp
@@ -7,152 +7,6 @@
 
 using namespace std;
 
-/*int opcode(int arr[] ){
-	int dec;
-	dec = (arr[0]*2) + (arr[1]);
-	return dec;
-}
-
-int opcode2(int arr[],int s,int e){
-	int dec2;
-	int start, end;
-	end = 31 - e;	//7
-	start = 31 - s; //9
-	dec2 = (arr[end]*4) + (arr[end +1]*2) + (arr[end +2]);
-	return dec2;
-}
-
-/*
- * bin is an array of 0 or 1
- * s is starting bit (inclusive)
- * e is ending bit (exclusive)
- * *//*
-string binToHex(int bin[], int s, int e){
-	string hex[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
-	int len = e - s;
-	int start = 31-e;
-	int end = 31-s;
-	string result = "";
-	while(len > 0){
-		int temp = 0;
-		switch(len){
-		case 1:
-			temp = bin[end];
-			break;
-		case 2:
-			temp = bin[end] + 2*bin[end - 1];
-			break;
-		case 3:
-			temp = bin[end] + 2*bin[end - 1] + 4*bin[end - 2];
-			break;
-		default:
-			temp = bin[end] + 2*bin[end - 1] + 4*bin[end - 2] + 8*bin[end - 3];
-			break;
-		}
-			result = hex[temp] + result;
-			len -= 4;
-			end -= 4;
-	}
-	return result;
-}*/
-/*
-*/
-
-
-/*int disp30(int arr[]){
-
-}*/
-
-
-
-/*
-typedef struct{
-	int32_t opcode3;
-	uint32_t rd;
-	uint32_t rs1;
-	union
-	{
-		struct
-		{
-			int i;
-			union
-			{
-				uint32_t rs2;
-				int32_t simm13;
-			} rs2;
-
-		} intg;
-
-		struct
-		{
-			uint32_t rs2;
-			uint32_t opf;
-		} fp;
-
-	} operand2;
-	} Arithmetic;
-
-	typedef struct{
-		uint32_t opcode2;
-
-		union
-		{
-			struct
-			{
-				uint32_t rd;
-				int32_t imm22;
-			} sethi;
-
-			struct
-			{
-			int32_t a;
-			int32_t cond;
-			int32_t disp22;
-			} branch;
-
-		} target;
-	} Branch;
-
-	typedef struct {
-		uint32_t opcode1;
-		union
-		{
-		Arithmetic a;
-		Branch b;
-		Call c;
-		} inst_type;
-	} decoded_instr;
-
-
-*/
-
-
-/**/
-/*
-typedef struct Inst {
-    unsigned short op;
-    union {
-        unsigned short rd;
-        struct {
-            unsigned short a;
-            unsigned short cond;
-        };
-    } dest;
-
-    unsigned short op2;
-    unsigned short op3;
-    unsigned short rs1;
-    unsigned short i;
-    unsigned short opf;
-    unsigned short asi;
-    unsigned short rs2;
-    unsigned short simm13;
-    unsigned short imm22;
-    unsigned short disp22;
-    unsigned short disp30;
-} Inst;
-*/
-
 typedef struct{
         int32_t disp30;
     } format1;
@@ -241,12 +95,6 @@ void Bicccond(inst x_inst){
         "BVC"
     };
 	
-    //string
-	/*cond = (arr[3] * 8) + (arr[4]*4) + (arr[5]*2) + (arr[6]);
-	printf(">>>>>-----------------------------------------------<<<<<\n");
-	cout << "\t" <<binToHex(arr,0,8) << " " << binToHex(arr,8,16) << " " << binToHex(arr,16,24) << " " << binToHex(arr,24,32) << "\t" ;
-	cout << Bicc[cond] << " " << "Address_to_jump " << "address+offset" << "\n";
-	printf(">>>>>-----------------------------------------------<<<<<\n");*/
 
 	cond = x_inst.inst_type.b.target.branch.cond;
 	temp  = x_inst.inst_type.b.target.branch.a << 2;
@@ -330,7 +178,7 @@ void op3two(inst x_inst){
 	uint32_t rd;
 	uint32_t rs1;
 	uint32_t rs2;
-	int i;
+	uint32_t i;
 	int k,l;
 	uint32_t opf;
 	uint32_t simm13;
@@ -360,63 +208,110 @@ void op3two(inst x_inst){
     rd = x_inst.inst_type.c.rd;
     rs1= x_inst.inst_type.c.rs1;
     op3= x_inst.inst_type.c.op3;
+	i  = x_inst.inst_type.c.operand2.intg.i; 
+	
+
+	string fpop2[]{
+		"51 FCMPs",
+		"52 FCMPd",
+		"53 FCMPq",
+		"55 FCMPEs",
+		"56 FCMPEd",
+		"57 FCMPEq"
+	};
 
     if(op3==0x34){
     	string fpop[] = {
-    		"01 FMOVs"
-			"05 FNEGs"
-			"09 FABSs"
-			"29 FSQRTs"
-			"2A FSQRTd"
-			"2B FSQRTq"
-			"41 FADDs"
-			"42 FADDd"
-			"43 FADDq"
-			"45 FSUBs"
-			"46 FSUBd"
-			"47 FSUBq"
-			"49 FMULs"
-			"4A FMULd"
-			"4B FMULq"
-			"4D FDIVs"
-			"4E FDIVd"
-			"4F FDIVq"
-			"69 FsMULd"
-			"6E FdMULq"
-			"C4 FiTOs"
-			"C6 FdTOs"
-			"C7 FqTOs"
-			"C8 FiTOd"
-			"C9 FsTOd"
-			"CB FqTOd"
-			"CC FiTOq"
-			"CD FsTOq"
-			"CE FdTOq"
-			"D1 FsTOi"
-			"D2 FdTOi"
+    		"01 FMOVs",
+			"05 FNEGs",
+			"09 FABSs",
+			"29 FSQRTs",
+			"2A FSQRTd",
+			"2B FSQRTq",
+			"41 FADDs",
+			"42 FADDd",
+			"43 FADDq",
+			"45 FSUBs",
+			"46 FSUBd",
+			"47 FSUBq",
+			"49 FMULs",
+			"4A FMULd",
+			"4B FMULq",
+			"4D FDIVs",
+			"4E FDIVd",
+			"4F FDIVq",
+			"69 FsMULd",
+			"6E FdMULq",
+			"C4 FiTOs",
+			"C6 FdTOs",
+			"C7 FqTOs",
+			"C8 FiTOd",
+			"C9 FsTOd",
+			"CB FqTOd",
+			"CC FiTOq",
+			"CD FsTOq",
+			"CE FdTOq",
+			"D1 FsTOi",
+			"D2 FdTOi",
 			"D3 FqTOi"
     	};
 
-    	printf("%s %s%d %d %s%d \n",cbc[l][k],RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));
+    	if(rd < 8) RegRd = "%f";
+		else if(rd <16) RegRd= "%f";
+		else if(rd <24) RegRd= "%f";
+		else if(rd <32) RegRd= "%f";
+
+		if(rs1 < 8) RegRs1 = "%f";
+		else if(rs1 <16) RegRs1= "%f";
+		else if(rs1 <24) RegRs1= "%f";
+		else if(rs1 <32) RegRs1= "%f";
+
+
+	  	if(rs2 < 8) RegRs2 = "%f";
+		else if(rs2 <16) RegRs2= "%f";
+		else if(rs2 <24) RegRs2= "%f";
+		else if(rs2 <32) RegRs2= "%f";
+
+
+    	k = x_inst.inst_type.c.operand2.fp.opf;
+    	// k == 09 is also with the below code
+    	if(k==01 || k==05  || k==41 || k==42 || k==43 || k==196 || k==198 || k==199 || k==200 || k==201 ||k==203 || k==204 || k==205 ||k==206 ||  k==209 || k==210 ||k==211){
+    		printf("%s %s%d %s%d \n",fpop2[k].c_str(),RegRs2.c_str(),(rs2),RegRd.c_str(),(rd));
+			return;
+    	}
+
+			printf("%s %s%d %s%d %s%d \n",fpop[k].c_str(),RegRd.c_str(),(rd%8),RegRs2.c_str(),(rs2%8),RegRs1.c_str(),(rs1%8));
+
+
+
     	return;
 
     }
     else if(op3==0x35){
-    	string fpop2[]{
-    		"51 FCMPs"
-			"52 FCMPd"
-			"53 FCMPq"
-			"55 FCMPEs"
-			"56 FCMPEd"
-			"57 FCMPEq"
-    	};
 
+    	if(rd < 8) RegRd = "%f";
+		else if(rd <16) RegRd= "%f";
+		else if(rd <24) RegRd= "%f";
+		else if(rd <32) RegRd= "%f";
+
+		if(rs1 < 8) RegRs1 = "%f";
+		else if(rs1 <16) RegRs1= "%f";
+		else if(rs1 <24) RegRs1= "%f";
+		else if(rs1 <32) RegRs1= "%f";
+
+		if(rs2 < 8) RegRs2 = "%f";
+		else if(rs2 <16) RegRs2= "%f";
+		else if(rs2 <24) RegRs2= "%f";
+		else if(rs2 <32) RegRs2= "%f";
+
+		printf("%s %s%d %s%d %s%d \n",fpop2[k].c_str(),RegRd.c_str(),(rd%8),RegRs2.c_str(),(rs2%8),RegRs1.c_str(),(rs1%8));
+		
     	return;
 
     }
 
     op354 = x_inst.inst_type.c.op3 >> 4;
-    op024 = (((1 << 5) - 1) & (op3));
+    op024 = (x_inst.inst_type.c.op3 << 2)>>2;
 
   	k = op354;
   	l = op024;
@@ -431,19 +326,104 @@ void op3two(inst x_inst){
 	else if(rs1 <24) RegRs1= "%l";
 	else if(rs1 <32) RegRs1= "%i";
 
-	if(i=0){
+	/*
+	//excptions
+	if(l==0 && k ==3){
+		if(i==0){printf("WRY %s%d %d %s%d \n",RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));};
+		else if(i==1){printf("WRASR %s%d %d %s%d \n",RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));};
+	}
+	else if (l == 1 && k ==3){
+		printf("WPRSR %s%d %d %s%d \n",RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));	
+	}
+	*/
+	i  = x_inst.inst_type.c.operand2.intg.i; 
+	if(i=1){
+		 rs2= x_inst.inst_type.c.operand2.intg.rs2.rs2;
 	  	if(rs2 < 8) RegRs2 = "%g";
 		else if(rs2 <16) RegRs2= "%o";
 		else if(rs2 <24) RegRs2= "%l";
 		else if(rs2 <32) RegRs2= "%i";
 
-		printf("%s %s%d %s%d %s%d \n",cbc[l][k],RegRd.c_str(),(rd%8),RegRs2.c_str(),(rs2%8),RegRs1.c_str(),(rs1%8));
+		printf("%s %s%d %s%d %s%d \n",cbc[l][k].c_str(),RegRs1.c_str(),(rs1%8),RegRs2.c_str(),(rs2%8),RegRd.c_str(),(rd%8));
 	}
-	else if(i=1){
-		printf("%s %s%d %d %s%d \n",cbc[l][k],RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));
+	else if(i=0){
+		simm13 = x_inst.inst_type.c.operand2.intg.rs2.simm13 << 2;
+		printf("%s %s%d %d %s%d \n",cbc[l][k].c_str(),RegRd.c_str(),(rd%8), simm13 ,RegRs1.c_str(),(rs1%8));
 	}
     return;
 }
+
+void op3one(inst x_inst){
+	uint32_t op3; 
+	uint32_t rd;
+	uint32_t rs1;
+	uint32_t rs2;
+	uint32_t i;
+	int k,l;
+	uint32_t opf;
+	uint32_t simm13;
+	uint32_t op354;
+	uint32_t op024;
+	string RegRd;
+	string RegRs1;
+	string RegRs2;
+	string cbc[][4] = {
+						{"LD"    ,"LDA"    ,"LDF"   ,"LDC"},
+					  	{"LDUB"  ,"LDUBA"  ,"LDFSR" ,"LDCSR"},
+			 			{"LDUH"  ,"LDUHA"  ,"NULL"  ,"NULL"},
+		        		{"LDD"   ,"LDDA"   ,"LDDF"  ,"LDDC"},
+		        		{"ST"    ,"STA"    ,"STF"   ,"STC"},
+		        		{"STB"   ,"STBA"   ,"STFSR" ,"STCSR"},
+		        		{"STH"   ,"STHA"   ,"STDFQ" ," STDCQ"},
+		        		{"STD"   ,"STDA"   ,"STDF"  ,"STDC"},
+		        		{"NULL"  ,"NULL"   ,"NULL"  , "NULL"}, 
+		        		{"LDSB"  ,"LDSBA"  ,"NULL"  ,"NULL"},
+		        		{"LDSH"  ,"LDSHA"  ,"NULL"  ,"NULL"},
+		        		{"NULL"  ,"NULL"   ,"NULL"  ,"NULL"},
+		        		{"NULL"  ,"NULL"   ,"NULL"  ,"NULL"},
+		        		{"LDSTUB","LDSTUBA","NULL"  ,"NULL"},
+		        		{"NULL"  ,"NULL"   ,"NULL"  ,"NULL"},
+				        {"SWAP"  ,"SWAPA"  ,"NULL"  ,"NULL"}
+    };
+    rd = x_inst.inst_type.c.rd;
+    rs1= x_inst.inst_type.c.rs1;
+    op3= x_inst.inst_type.c.op3;
+	i  = x_inst.inst_type.c.operand2.intg.i; 
+
+    op354 = x_inst.inst_type.c.op3 >> 4;
+    op024 = (x_inst.inst_type.c.op3 << 2)>>2;
+
+  	k = op354;
+  	l = op024;
+
+  	if(rd < 8) RegRd = "%g";
+	else if(rd <16) RegRd= "%o";
+	else if(rd <24) RegRd= "%l";
+	else if(rd <32) RegRd= "%i";
+
+	if(rs1 < 8) RegRs1 = "%g";
+	else if(rs1 <16) RegRs1= "%o";
+	else if(rs1 <24) RegRs1= "%l";
+	else if(rs1 <32) RegRs1= "%i";
+
+	i  = x_inst.inst_type.c.operand2.intg.i; 
+	if(i=0){
+		 rs2= x_inst.inst_type.c.operand2.intg.rs2.rs2;
+	  	if(rs2 < 8) RegRs2 = "%g";
+		else if(rs2 <16) RegRs2= "%o";
+		else if(rs2 <24) RegRs2= "%l";
+		else if(rs2 <32) RegRs2= "%i";
+
+		printf("%s [%s%d + %s%d], %s%d \n",cbc[l][k].c_str(),RegRs1.c_str(),(rs1%8),RegRs2.c_str(),(rs2%8),RegRd.c_str(),(rd%8));
+	}
+	else if(i=1){
+		simm13 = x_inst.inst_type.c.operand2.intg.rs2.simm13 << 2;
+		printf("%s [%s%d + %d], %s%d \n",cbc[l][k].c_str(),RegRs1.c_str(),(rs1%8), simm13,RegRd.c_str(),(rd%8));
+	}
+    return;
+}
+
+
 
 void disassemble(inst x_inst) {
 	uint32_t r;		// random which will be used to store some imp values
@@ -491,7 +471,7 @@ void disassemble(inst x_inst) {
 			}
 
 			else if(eop == 7){
-
+				CBccccond(x_inst);
 			}
 
 			break;
@@ -504,7 +484,8 @@ void disassemble(inst x_inst) {
 
 		case 3:
 			//printf("we are in opcode 2 or 3\n");
-			
+			eop = x_inst.inst_type.c.op3 >> 4;
+			op3one(x_inst);
 			break;
 
 
@@ -616,11 +597,14 @@ void decode(uint32_t instr){
 
 int main() {
 
-    uint32_t instructions[] = {0x40025642, 0x1110036f};
+    uint32_t instructions[] = {0x1a80000c,0xba274001, 0x92004009};
+    // ,0x03bffebf,0x40025642,0xee4e6002
+    //0xc208e001,0xa200e001,8216c002
+    //
 
-    for(int i = 0; i < NUM_INSTRUCTIONS; i++) {
-    	printf("%08jx ", (uintmax_t)instructions[i]);
-        decode(instructions[i]);
+    for(int j = 0; j < 3; j++) {
+    	printf("%08jx ", (uintmax_t)instructions[j]);
+        decode(instructions[j]);
     }
 
     return 0;
